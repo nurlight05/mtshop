0f779850698209e6cc2a27223f7093e3c3380e14
nurlight05 <nurlight05@gmail.com>
2021-01-07 21:18:30 +0600

    changed

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -21,6 +21,7 @@
                 font-family: 'Nunito';
             }
         </style>
+        {{-- sdcsdcdcscsdcdscsc --}}
     </head>
     <body class="antialiased">
         <div class="relative flex items-top justify-center min-h-screen bg-gray-100 dark:bg-gray-900 sm:items-center sm:pt-0">
